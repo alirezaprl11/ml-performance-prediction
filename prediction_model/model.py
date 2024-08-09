5896248b00585cea096991cb7d45129690cf20f4
Alireza Pourali <124391726+alirezaprl11@users.noreply.github.com>
2024-08-09 18:18:50 -0400

    Update model.py

--- prediction_model/model.py
+++ prediction_model/model.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 import numpy as np
 import os
 
-class Model:
+class Model(tf.keras.Model):
     """Model class"""
-    def __init__(self,inputs,targets,learning_rate,reg_constant,dropout_rate,
-                 num_neurons,lr_initial,lr_decay_step,batch_size,model_name):
+    def __init__(self, inputs, targets, learning_rate, reg_constant, dropout_rate,
+                 num_neurons, lr_initial, lr_decay_step, batch_size, model_name):
 
+        super(Model, self).__init__()
         self.inputs = inputs
         self.targets = targets
         self.learning_rate = learning_rate
@@ -19,161 +20,122 @@ class Model:
         self.batch_size = batch_size
         self.model_name = model_name
 
-        self._prediction = None
-        self._loss = None
-        self._train_op = None
-        self._summary_op = None
+        self.global_step = tf.Variable(0, trainable=False, name='global_step')
 
-        self.istraining = tf.placeholder(tf.bool,
-                                         shape=None,
-                                         name='model_istraining')
+        # Building the model layers
+        self.fc_layers = [tf.keras.layers.Dense(units=n, activation='relu',
+                                                kernel_regularizer=tf.keras.regularizers.l2(reg_constant))
+                          for n in num_neurons]
+        self.dropout = tf.keras.layers.Dropout(rate=dropout_rate)
+        self.output_layer = tf.keras.layers.Dense(units=1, activation='relu', use_bias=False)
 
-        self.global_step = tf.get_variable('global_step',
-                                           initializer=tf.constant(0),
-                                           trainable=False)
-
-
-    @property
-    def prediction(self):
-        """Funtion to build the model and generate a prediction
-        returns property
-        """
-        if self._prediction is None:
-            data_dim = self.inputs.shape[1]
-
-            fcLayer = self.inputs
-            # Some fully connected layers
-            for layer in range(0,self.num_layers):
-                name_scope = 'layer_fc%d' %layer
-                with tf.variable_scope(name_scope) as scope:
-                    fcLayer = tf.layers.dense(
-                            inputs = fcLayer,
-                            units = self.num_neurons[layer],
-                            activation = tf.nn.relu,
-                            kernel_regularizer = tf.contrib.layers.l2_regularizer(self.reg_constant),
-                            use_bias = True)
-
-            # Dropout only after last layer
-            fcLayer = tf.layers.dropout(inputs=fcLayer,
-                                        rate=self.dropout_rate,
-                                        training=self.istraining)
-
-            # Prediction. Relu for preventing negative results
-            output = tf.layers.dense(
-                    inputs = fcLayer,
-                    units = 1,
-                    activation = tf.nn.relu,
-                    use_bias = False)
-
-            self._prediction = tf.reshape(output, [-1], name='model_prediction')
-        return self._prediction
+        # Define the loss function
+        self.loss_fn = tf.keras.losses.MeanSquaredError()
 
 
-    @property
-    def loss(self):
-        """Function that generates the loss, returns property"""
-        if self._loss is None:
-            self._loss = (tf.losses.get_regularization_loss()
-                         + tf.losses.mean_squared_error(
-                               labels=tf.log(1+self.targets),
-                               predictions=tf.log(1+self.prediction)))
+    def get_config(self):
+        config = super(Model, self).get_config()
+        config.update({
+            'inputs': self.inputs,
+            'targets': self.targets,
+            'learning_rate': self.learning_rate,
+            'reg_constant': self.reg_constant,
+            'dropout_rate': self.dropout_rate,
+            'num_neurons': self.num_neurons,
+            'lr_initial': self.lr_initial,
+            'lr_decay_step': self.lr_decay_step,
+            'batch_size': self.batch_size,
+            'model_name': self.model_name
+        })
+        return config
 
-            # self._loss = (tf.losses.get_regularization_loss()
-            #               + tf.losses.mean_squared_error(
-            #                     labels=self.targets,
-            #                     predictions=self.prediction))
-        return self._loss
+    
+    @classmethod
+    def from_config(cls, config):
+        return cls(
+            inputs=config['inputs'],
+            targets=config['targets'],
+            learning_rate=config['learning_rate'],
+            reg_constant=config['reg_constant'],
+            dropout_rate=config['dropout_rate'],
+            num_neurons=config['num_neurons'],
+            lr_initial=config['lr_initial'],
+            lr_decay_step=config['lr_decay_step'],
+            batch_size=config['batch_size'],
+            model_name=config['model_name']
+        )
 
 
-    @property
-    def train_op(self):
-        """Function that generates the train operation, returns property"""
-        if self._train_op is None:
-            opt = tf.train.AdamOptimizer(
-                    learning_rate=self.learning_rate,
-                    epsilon=.1)
-            self._train_op = opt.minimize(
-                    self.loss,
-                    global_step=self.global_step)
-        return self._train_op
+    def call(self, inputs, training=False):
+        x = inputs
+        for fc_layer in self.fc_layers:
+            x = fc_layer(x)
+        if training:
+            x = self.dropout(x, training=training)
+        return tf.reshape(self.output_layer(x), [-1])
 
+    def compute_loss(self):
+        log_targets = tf.math.log(1 + self.targets)
+        log_predictions = tf.math.log(1 + self.call(self.inputs, training=True))
+        mse_loss = tf.reduce_mean(self.loss_fn(log_targets, log_predictions))
+        reg_loss = tf.reduce_sum(self.losses)  # This captures the regularization losses
+        return mse_loss + reg_loss
 
-    @property
-    def summary_op(self):
-        """Function to write the summary, returns property"""
-        if self._summary_op is None:
-            tf.summary.scalar("loss", self.loss)
-            tf.summary.scalar("learning_rate", self.learning_rate)
-            tf.summary.histogram("histogram_loss", self.loss)
-            self._summary_op =  tf.summary.merge_all()
-        return self._summary_op
-
+    def train_step(self):
+        with tf.GradientTape() as tape:
+            loss = self.compute_loss()
+        gradients = tape.gradient(loss, self.trainable_variables)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, epsilon=0.1)
+        optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        self.global_step.assign_add(1)
+        return loss
 
     def train(self, traindata, trainlabel, testdata, testlabel, num_train_steps):
-        """Train the model for a number of steps, save checkpoints, write
-        graph and loss for tensorboard
-        Inputs:
-            traindata
-            trainlabel
-            testdata
-            testlabel
-            num_train_steps
-        """
-
+        """Train the model for a number of steps, save checkpoints, write graph and loss for tensorboard"""
         print(os.path.abspath('./'))
-        saver = tf.train.Saver()
-
-        initial_step = 0
-
-        try:
-            os.mkdir('./checkpoints/%s' %self.model_name)
-        except:
-            pass
+        checkpoint_dir = f'./checkpoints/{self.model_name}'
+        os.makedirs(checkpoint_dir, exist_ok=True)
+        checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 
         num_datapoints = traindata.shape[0]
-        list_datapoints = np.arange(0,num_datapoints)
-        num_batches = np.int(np.ceil(num_datapoints/self.batch_size))
+        list_datapoints = np.arange(num_datapoints)
+        num_batches = int(np.ceil(num_datapoints / self.batch_size))
 
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            ckpt = tf.train.get_checkpoint_state(os.path.dirname('./checkpoints/%s/checkpoint' %self.model_name))
+        # Set up TensorBoard
+        train_summary_writer = tf.summary.create_file_writer(f'./logs/train/{self.model_name}')
+        test_summary_writer = tf.summary.create_file_writer(f'./logs/test/{self.model_name}')
+        checkpoint = tf.train.Checkpoint(optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate), model=self)
 
-            # if that checkpoint exists, restore from checkpoint
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
+        for epoch in range(num_train_steps):
+            np.random.shuffle(list_datapoints)
+            avg_loss = 0
+            for i in range(num_batches):
+                batch_indices = list_datapoints[i * self.batch_size: min((i + 1) * self.batch_size, num_datapoints)]
+                self.inputs = traindata[batch_indices, :]
+                self.targets = trainlabel[batch_indices]
+                self.learning_rate = self.lr_initial * 2 ** (-np.floor(epoch / self.lr_decay_step))
+                loss = self.train_step()
+                avg_loss += loss / num_batches
 
-            writer_train = tf.summary.FileWriter('./graphs/prediction/train/%s' %self.model_name, sess.graph)
-            writer_test = tf.summary.FileWriter('./graphs/prediction/test/%s' %self.model_name, sess.graph)
+            with train_summary_writer.as_default():
+                tf.summary.scalar('loss', avg_loss, step=epoch)
 
-            initial_step = self.global_step.eval()
+            self.inputs = testdata
+            self.targets = testlabel
+            test_loss = self.compute_loss()
+            with test_summary_writer.as_default():
+                tf.summary.scalar('test_loss', test_loss, step=epoch)
 
-            for epoch in range(initial_step, initial_step + num_train_steps):
-                np.random.shuffle(list_datapoints)
-                avg_loss = 0
-                for i in range(0,num_batches):
-                    batch = list_datapoints[i*self.batch_size:min((i+1)*self.batch_size,num_datapoints)]
-                    _, loss, summary = sess.run(
-                            [self.train_op, self.loss, self.summary_op],
-                            feed_dict={
-                                    self.inputs: traindata[batch,:],
-                                    self.targets: trainlabel[batch],
-                                    self.learning_rate: self.lr_initial*2**(-np.floor(epoch/self.lr_decay_step)),
-                                    self.istraining: True})
-                    avg_loss += loss/num_batches
-                writer_train.add_summary(summary, global_step=epoch)
+            checkpoint.save(file_prefix=checkpoint_prefix)
 
-                testloss, testsummary = sess.run(
-                        [self.loss,self.summary_op],
-                        feed_dict={
-                                self.inputs: testdata,
-                                self.targets: testlabel,
-                                self.learning_rate: self.lr_initial*2**(-np.floor(i/self.lr_decay_step)),
-                                self.istraining: False})
-                writer_test.add_summary(testsummary, global_step=epoch)
-                saver.save(sess, './checkpoints/%s/prediction' %self.model_name, epoch)
-                if epoch%10==0:
-                    print('Epoch {}: Train loss {:.3f}, Test loss {:.3f}'.format(epoch, avg_loss, testloss))
+            if epoch % 10 == 0:
+                print(f'Epoch {epoch}: Train loss {avg_loss:.3f}, Test loss {test_loss:.3f}')
 
+        train_summary_writer.close()
+        test_summary_writer.close()
 
-            writer_train.close()
-            writer_test.close()
+# Example usage:
+# Define your data and parameters
+# model = Model(inputs, targets, learning_rate, reg_constant, dropout_rate, num_neurons,
+#               lr_initial, lr_decay_step, batch_size, model_name)
+# model.train(traindata, trainlabel, testdata, testlabel, num_train_steps)
